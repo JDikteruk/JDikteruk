f4888b4831f15fc725f18266136ed1870c1107ab
Jon Dikteruk <jonathan.dikteruk@gmail.com>
2022-04-22 20:15:18 -0400

    Contact form finally works!!!

---
+++ js/email.js
@@ -0,0 +1,14 @@
+function sendEmail (){
+    var tempParams = {
+        name:document.getElementById("nameInput").value,
+        company:document.getElementById("company").value,
+        phone:document.getElementById("phone").value,
+        email:document.getElementById("email").value,
+        msg:document.getElementById("msg").value
+    };
+
+    emailjs.send('service_2cs2j5q','template_qpszt7m', tempParams)
+    .then(function(res){
+        console.log(res.status);
+    })
+}
